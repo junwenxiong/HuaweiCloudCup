4ce5e78e2020f22154b91abc1f14505147053541
woshihaozhu <358975248@qq.com>
2020-11-10 12:23:31 +0800

    modify train_mixed.sh file

--- train_mixed.sh
+++ train_mixed.sh
@@ -1,3 +1,3 @@
 CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch --nproc_per_node=2 train_mixed.py --learn-rate 0.001 --weight-decay 0 \
---batch-size 2 --gpu-ids 0,1 --backbone unet --dataset ./data/ \
---base-size 256 --crop-size 256 --epochs 100 --batch-size 48
+--batch-size 16 --gpu-ids 0,1 --backbone unet --dataset /data/home/zy/huawei \
+--base-size 256 --crop-size 256 --epochs 100
